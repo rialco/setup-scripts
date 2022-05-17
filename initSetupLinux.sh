279a912b4a34c811c50b794b677087b15e65115b
Ricardo Alvarez <ralvarezcm@gmail.com>
2022-05-17 18:52:20 -0500

    feat: update linux script & readme

--- initSetupLinux.sh
+++ initSetupLinux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+printf "==> Beginning setup for new debian-based machine \n\n"
 
-echo 'Beginning setup for new debian-based machine'
+BLUE='\033[0;34m';
+NC='\033[0m';
 
 # Update base packages
 printf "Updating system..."
@@ -10,19 +12,78 @@ apt autoremove -y
 printf "Finished update \n\n"
 
 # Install essentials
-printf "Installing essentials... (vim, wget, gpg) \n\n"
-apt install vim wget gpg -qq
+echo -e "${BLUE}==> Installing essentials (vim, wget, gpg, zsh)${NC}"
+
+apt install vim wget gpg zsh -qq
 printf "\n\n"
 
-# Install docker 
+echo -e "${BLUE}==> Do you want to set zsh as the default shell?${NC}"
+select yn in "Yes" "No"; 
+do
+    case $yn in
+        Yes)
+		chsh -s /usr/bin/zsh
+		break;;
+		No) 
+		break;;
+    esac
+done
 
-echo "Do you want to install Docker?"
+echo -e "${BLUE}==> Do you want to install Oh my zsh, zsh syntax highlight & auto-suggestions?${NC}"
+select yn in "Yes" "No"; 
+do
+    case $yn in
+        Yes)
+		sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+		echo "Finished installing Oh my zsh";
+		echo "==> Installing PowerLevel10k";
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
+		echo "Powerlevel10k finished installing";
+		echo "==> Installing Syntax Highlighting";
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
+		echo "Sytanx highlighting finished installing";
+		echo "==> Installing auto-suggestions";
+		git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+		echo "Auto-suggestions finished installing";
 
+		echo "==> Starting P10K config";
+		p10k configure;
+
+		echo "==> Editing .zshrc file to include recently downloaded changes";
+		sed 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc;
+		sed 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/' ~/.zshrc;
+		sed 's/# ENABLE_CORRECTION="true"/ENABLE_CORRECTION="true"/' ~/.zshrc;
+		echo 'export EDITOR="code -w"' >> ~/.zshrc;
+		break;;
+		No) 
+		break;;
+    esac
+done
+
+echo -e "${BLUE}==> Do you want to install Visual Studio Code?${NC}"
 select yn in "Yes" "No";
 do
 	case $yn in
 	Yes)
-	printf "Installing docker";
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+	sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+	rm -f packages.microsoft.gpg
+
+	apt install apt-transport-https
+	apt update
+	apt install code # or code-insiders
+	break;;
+	No)
+	break;;
+	esac;
+done;
+
+echo -e "${BLUE}==> Do you want to install Docker?${NC}"
+select yn in "Yes" "No";
+do
+	case $yn in
+	Yes)
 	apt-get remove docker docker-engine docker.io containerd runc -y;
 	apt-get install ca-cert curl gnupg lsb-release -y;
 
