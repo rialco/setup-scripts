d12b27d6c92a59f436af220bc648a3c9b3f63b4e
ricardo <ralvarezcm@gmail.com>
2025-02-24 16:40:25 -0500

    refactor: update naming & debian script

--- debian_setup.sh
+++ debian_setup.sh
@@ -17,17 +17,11 @@ echo -e "${BLUE}==> Installing essentials (vim, wget, gpg, zsh)${NC}"
 apt install vim wget gpg zsh -qq
 printf "\n\n"
 
-echo -e "${BLUE}==> Do you want to set zsh as the default shell?${NC}"
-select yn in "Yes" "No"; 
-do
-    case $yn in
-        Yes)
-		chsh -s /usr/bin/zsh
-		break;;
-		No) 
-		break;;
-    esac
-done
+echo "==> Setting ZSH as the default shell";
+chsh -s /usr/bin/zsh
+echo "ZSH set as the default shell successfully ";
+
+printf "\n\n"
 
 echo -e "${BLUE}==> Do you want to install Oh my zsh, zsh syntax highlight & auto-suggestions?${NC}"
 select yn in "Yes" "No"; 
@@ -53,49 +47,33 @@ do
 		sed 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc;
 		sed 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/' ~/.zshrc;
 		sed 's/# ENABLE_CORRECTION="true"/ENABLE_CORRECTION="true"/' ~/.zshrc;
-		echo 'export EDITOR="code -w"' >> ~/.zshrc;
 		break;;
 		No) 
 		break;;
     esac
 done
 
-echo -e "${BLUE}==> Do you want to install Visual Studio Code?${NC}"
-select yn in "Yes" "No";
-do
-	case $yn in
-	Yes)
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-	sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-	rm -f packages.microsoft.gpg
-
-	apt install apt-transport-https
-	apt update
-	apt install code # or code-insiders
-	break;;
-	No)
-	break;;
-	esac;
-done;
-
 echo -e "${BLUE}==> Do you want to install Docker?${NC}"
 select yn in "Yes" "No";
 do
 	case $yn in
 	Yes)
-	apt-get remove docker docker-engine docker.io containerd runc -y;
-	apt-get install ca-cert curl gnupg lsb-release -y;
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    apt-get update
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	  chmod a+r /etc/apt/keyrings/docker.asc
 
-	curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-	echo \
-  	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  	$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null;
-
-	apt-get update -qq;
-	apt-get install docker-ce docker-ce-cli containerd.io -qq;
-	break;;
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    break;;
 	No)
 	break;;
 	esac;
